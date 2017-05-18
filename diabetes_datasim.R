26c0d14b6eddafbf728a336c133d8b2eb3209696
tobiolatunji <onemediastudios@gmail.com>
2017-05-17 19:39:16 -0700

    end datasim

--- diabetes_datasim.R
+++ diabetes_datasim.R
@@ -6,259 +6,29 @@ summary(data2)
 
 logistic <- function(t) 1 / (1 + exp(-t))
 
-# simulate factor/categorical variable
-
-x <- sample( LETTERS[1:4], 10000, replace=TRUE, prob=c(0.1, 0.2, 0.65, 0.05) )
-x <- sample( c("A","B","C","D"), 10000, replace=TRUE, prob=c(0.1, 0.2, 0.65, 0.05) )
-prop.table(table(x))
-
-x <- rep( c("A","B","C","D"), 10000*c(0.1,0.2,0.65,0.05) )
-x <- sample(x, 10000) 
-
-sample(1:4,10000,rep=TRUE,prob=c(.1,.2,.3,.4))
-
-
-mycat <- cut(runif(10000), c(0, 0.1, 0.3, 0.6, 1))
-prop.table(table(mycat))
-mycat <- cut(runif(10000), c(0, 0.1, 0.3, 0.6, 1), labels=FALSE)
-
-# simulate random multinomial categorical correlated data
-library(GenOrd)
-set.seed(1)
-# Sets the marginals.
-# The values are cumulative so for the first variable the first marginal will be .1, the second is .2, the third is .3, and the fourth is .4
-marginal <- list(c(0.1,0.3,0.6),c(0.4,0.7,0.9))
-# Checks the lower and upper bounds of the correlation coefficients.
-corrcheck(marginal)
-# Sets the correlation coefficients
-R <- matrix(c(1,-0.6,-0.6,1),2,2) # Correlation matrix
-n <- 1000
-##Selects and ordinal sample with given correlation R and given marginals.
-m <- ordsample(n, marginal, R)
-##compare it with the pre-defined R
-cor(m)
-prop.table(table(m[,1],m[,2]))
-margin.table(table(m[,1],m[,2]),1)
-
-chisq.test(m)
-
-gbar < - tapply(m[,1], list(m[,1], m[,2]), length)
-
-par(mfrow=c(1,1))
-barplot(gbar, beside=T, col=cm.colors(4), main="Example Bar Chart of Counts by Group",xlab="Group",ylab="Frequency")
-
-
-
-library(manipulate)
-N <- 1e3
-logistic <- function(t) 1 / (1 + exp(-t))
-
-simLOS <- function(N) {
-  AGE  <- sample(1:94, N, replace=T)
-  COPD <- runif(N)< .12*logistic((AGE-50)/10)
-  ASTH <- runif(N)< .095
-  HRTD <- runif(N)< .19*logistic((AGE-50)/10)
-  DIAB <- runif(N)< .08*logistic((AGE-50)/10)
-  CANC <- runif(N)< .19*logistic((AGE-40)/10)
-  LOWR <- !(COPD+ASTH+HRTD+DIAB+CANC)
-  FLUSHOT <- runif(N)< .34 + .5*(COPD+ASTH+HRTD+DIAB+CANC)
-  pflu  <- ifelse(FLUSHOT,.05,.20)
-  FLU  <- runif(N)<pflu
-  LOS <- 4 + COPD*8 + ASTH*6 +HRTD*6 + DIAB*4 + CANC*5 +FLU*2 +round(rnorm(N,sd=2,mean=0),digits=0)
-  LOS[LOS<1]<- 1
-  data.frame(AGE,COPD,ASTH,HRTD,DIAB,CANC,LOWR,FLUSHOT,FLU,LOS)    
-}
-datafr<- (simLOS(N))
-
-
-head(datafr)
-
-
-
-# simulate binomial variable
-
-a <- 1
-amx <- ifelse(runif(10000)<.3,-a,a)
-sum(amx==1)
-
-a <- 10
-amx <- sample(c(-a,a),12,replace=TRUE)
-sum(amx==10)
-
-a <- 6
-ifelse(rbinom(4,1,.5),-a,a)
-
-
-
-library(bindata)
-## Construct a binary correlation matrix
-rho <- 0.7905694
-m <- matrix(c(1,rho,rho,1), ncol=2)   
-## Simulate 10000 x-y pairs, and check that they have the specified
-## correlation structure
-x <- rmvbin(1e5, margprob = c(0.5, 0.5), bincorr = m) 
-cor(x)
-
-
-# simulate Random Multivariate Correlated Data (Continuous Variables)
-
-R = matrix(cbind(1,.80,.2,  .80,1,.7,  .2,.7,1),nrow=3)
-U = t(chol(R))
-nvars = dim(U)[1]
-numobs = 100000
-set.seed(1)
-random.normal = matrix(rnorm(nvars*numobs,0,1), nrow=nvars, ncol=numobs);
-X = U %*% random.normal
-newX = t(X)
-raw = as.data.frame(newX)
-orig.raw = as.data.frame(t(random.normal))
-names(raw) = c("response","predictor1","predictor2")
-cor(raw)
-plot(head(raw, 100))
-plot(head(orig.raw,100))
-
-
-library(polycor) 
-sim1 <- function(thresh=0.5, r=0.3) { 
-  x <- rmvnorm(1000,c(0,0),matrix(c(1,r,r,1), nr=2)) 
-  x[x>thresh] <- 2 
-  x[x<2] <- 1 
-  polychor(x[,1], x[,2]) 
-} 
-
-tr <- double(100) 
-for(i in 1:100) tr[i] <- sim1() 
-summary(tr) 
-
-?manipulate
-
-# CORRELATION OF CATEGORICALS
-# example data
-set.seed(1)
-DF <- data.frame(x=sample(c("Y","N"),100,T),y=sample(c("Y","N"),100,T))
-
-# how to get correlation
-DF[] <- lapply(DF,as.integer)
-cor(DF)
-#            x          y
-# x  1.0000000 -0.0369479
-# y -0.0369479  1.0000000
-
-# visualize it
-library(corrplot)
-corrplot(cor(DF))
-
-
-?aov
-?ICC 
-
-t1 = c(164, 172, 168, 177, 156, 195)
-t2 = c(178, 191, 197, 182, 185, 177)
-t3 = c(175, 193, 178, 171, 163, 176)
-t4 = c(155, 166, 149, 164, 170, 168)
-
-val = c(t1, t2, t3, t4)
-fac = gl(n=4, k=6, labels=c('type1', 'type2', 'type3', 'type4'))
-
-aov1 = aov(val ~ fac)
-summary(aov1)
-
-
-
-df <- transform(df, score = 0.01 * (aerobicCap - mean(aerobicCap)) + 0.04 * 
-                  (energyIntake - mean(energyIntake))+ 481 * (bmi - mean(bmi)) - 2.1)
-df$prob <- logistic(df$score)
-
-# The outcome is then masked into a range of outcome for high and low scores
-df$scoreMath <- round(ifelse(df$prob > runif(N), rnorm(N,mean = 94, sd= 4),
-                             rnorm(N,mean = 45, sd= 3)),2)
-
-## Helper function to calculate cooefficients
-library(manipulate)
-manipulate({
-  df1 <- transform(df, score = 10^a * (aerobicCap - mean(aerobicCap)) + 10^b * 
-                     (energyIntake - mean(energyIntake)) + 10^c * (bmi - mean(bmi)) - 2.1)
-  df1$prob <- logistic(df$score)
-  hist(df1$prob, breaks=50)
-}, a=slider(-9, 9, step=0.1, initial = 0), b=slider(-9, 9, step=0.1, initial = 0), 
-c=slider(-9, 9, step=0.1, initial = 0))
-
-
-# correlated continuous variables
-R = matrix(cbind(1,.80,.2,  .80,1,.7,  .2,.7,1),nrow=3)
-U = t(chol(R))
-nvars = dim(U)[1]
-numobs = 100000
-set.seed(1)
-random.normal = matrix(rnorm(nvars*numobs,0,1), nrow=nvars, ncol=numobs);
-X = U %*% random.normal
-newX = t(X)
-raw = as.data.frame(newX)
-orig.raw = as.data.frame(t(random.normal))
-names(raw) = c("response","predictor1","predictor2")
-cor(raw)
-plot(head(raw, 100))
-plot(head(orig.raw,100))
-
-
-
-R = matrix(cbind(1,.32,.19,.47,-.01,-0.01,.07,.22,  
-                 .32,1,.06,.27,-.01,-.002,.04,.15,  
-                 .19,.06,1,.39,-.02,-.04,-.07,.07,
-                 .47,.27,.39,1,.05,.01,.06,.26,
-                 -0.01,-0.01,-0.02,0.05,1,0.09,0.11,0.09,
-                 -0.01,-0.002,-0.04,0.01,0.09,1,0.27,0.06,
-                 0.07,0.034,-0.07,0.06,0.11,0.27,1,0.1,
-                 0.22,0.15,0.07,0.26,0.09,0.06,0.1,1
-                 ),
-           nrow=8)
-U = t(chol(R))
-nvars = dim(U)[1]
-numobs = 10000
-set.seed(1)
-random.normal = matrix(rpois(nvars*numobs,1.5), nrow=nvars, ncol=numobs);
-X = U %*% random.normal
-newX = t(X)
-raw = as.data.frame(newX)
-orig.raw = as.data.frame(t(random.normal))
-names(raw) = c('time_in_hospital','num_lab_procedures','num_procedures','num_medications',
-               'number_outpatient','number_emergency','number_inpatient','number_diagnoses')
-cor(raw)
-plot(head(raw, 100))
-plot(head(orig.raw,100))
-
-num_procedures <- rpois(10000,0.8)
-
-set.seed(1)
-number_diagnoses <- floor(raw$number_diagnoses*2*logistic((age-2.5)/4))
-number_diagnoses <- ifelse(raw$number_diagnoses<10,rnorm(10000,7.4,.8)*8*logistic((age-2.5)/8),floor(raw$number_diagnoses*1*logistic((age-2.5)/4)))
-number_diagnoses <- ifelse(raw$number_diagnoses<10,raw$number_diagnoses*2,floor(rpois(10000,0.12)*8*logistic((age-1)/4)))
-hist(number_diagnoses)
-plot(age,number_diagnoses)
-plot(age,raw$number_diagnoses*2)
-hist(data2$number_diagnoses)
-hist(raw$number_diagnoses*2)
-sd(data2$number_diagnoses)
-mean(data2$number_diagnoses)
-g <- ggplot(data2, aes(x=age, y=number_diagnoses))
-g + geom_boxplot()
-
- 
-number_diagnoses
 
 ########################################################
 
 # start datasim
 
-N <- 1e6
-set.seed(123)
+N <- 1e5
 
 generate_dataset <- function(N) {
+  set.seed(123)
   age <- sample(c("0-10","11-20","21-30","31-40","41-50","51-60","61-70","71-80","81-90","91-100"),
                 N, replace=T, prob=c(0.001, 0.007, 0.016, 0.037, 0.095, 0.17, 0.221, 0.257, 0.169, 0.027))
+  age_score <- sample(1:10, N, replace=T, prob=c(0.001, 0.007, 0.016, 0.037, 0.095, 0.17, 0.221, 0.257, 0.169, 0.027))
+  age <- ifelse(age_score==1,"0-10",(ifelse(age_score==2,"11-20",(ifelse(age_score==3,"21-30",
+                    (ifelse(age_score==4,"31-40",(ifelse(age_score==5,"41-50",
+                    (ifelse(age_score==6,"51-60",(ifelse(age_score==7,"61-70",
+                    (ifelse(age_score==8,"71-80",(ifelse(age_score==9,"81-90","91-100")))))))))))))))))
   gender <- sample(c('Female','Male'), N, replace = T, prob = c(0.538,0.462))
+  #gender_score <- sample(1:2, N, replace = T, prob = c(0.538,0.462))
+  #gender <- ifelse(gender_score==1,'Female','Male')
   race <- sample(c('AfricanAmerican','Asian','Caucasian','Hispanic','Other'), N,
                  replace = T,prob = c(0.189,0.006,0.748,0.02,0.037))
+  #### generate correlation matrix of 8 numeric variables
+  # https://www.r-bloggers.com/simulating-random-multivariate-correlated-data-continuous-variables/
   R = matrix(cbind(1,.32,.19,.47,-.01,-0.01,.07,.22,  
                    .32,1,.06,.27,-.01,-.002,.04,.15,  
                    .19,.06,1,.39,-.02,-.04,-.07,.07,
@@ -271,96 +41,59 @@ generate_dataset <- function(N) {
   U = t(chol(R))
   nvars = dim(U)[1]
   numobs = N
-  set.seed(1)
-  random.normal = matrix(rnorm(nvars*numobs,0,1), nrow=nvars, ncol=numobs);
+  random.normal = matrix(rpois(nvars*numobs,1.2), nrow=nvars, ncol=numobs);
   X = U %*% random.normal
   newX = t(X)
   raw = as.data.frame(newX)
   names(raw) = c('time_in_hospital','num_lab_procedures','num_procedures','num_medications',
                  'number_outpatient','number_emergency','number_inpatient','number_diagnoses')
-  time_in_hospital <- raw$time_in_hospital*1.8*logistic((age-4)/.7)
-  num_lab_procedures <- raw$num_lab_procedures*17.5*logistic((age-0)/.2)
-  num_procedures  <- ifelse(raw$num_procedures>0, floor(raw$num_procedures*0.9*logistic((age-4)/.8)),raw$num_procedures+1)
-  num_medications <- floor(raw$num_medications*9*logistic((age-2.5)/.8))
-  number_outpatient <- rpois(N,0.2)*14*logistic((age-3.5)/1.2)
-  number_emergency <- rpois(N,0.2)*24*logistic((age-5)/2)
-  number_inpatient <- rpois(N,0.18)*8*logistic((age-1)/4)
-  number_diagnoses <- ifelse(raw$number_diagnoses<10,raw$number_diagnoses*2,floor(rpois(N,0.12)*8*logistic((age-1)/4)))
-  cont_vars <- data.frame(time_in_hospital,num_lab_procedures,num_procedures,num_medications,
-                           number_outpatient,number_emergency,number_inpatient,number_diagnoses)
-  #cor(cont_vars)
+  time_in_hospital <- ceiling(raw$time_in_hospital*1.8*logistic((age_score-3.5)/.7))
+  num_lab_procedures <- ceiling(raw$num_medications*17.5*logistic((age_score-0)/.2))
+  num_procedures  <- ceiling(ifelse(raw$num_procedures>0, floor(raw$num_procedures*0.9*logistic((age_score-4)/.8)),raw$num_procedures+1))
+  num_medications <- ceiling(raw$num_medications*9*logistic((age_score-2.5)/.8))
+  number_outpatient <- ceiling(rpois(N,0.2)*14*logistic((age_score-3)/1.2))
+  number_emergency <- ceiling(rpois(N,0.2)*24*logistic((age_score-5)/2))
+  number_inpatient <- ceiling(rpois(N,0.18)*8*logistic((age_score-1)/4))
+  number_diagnoses <- ceiling(ifelse(raw$number_diagnoses<10,raw$number_diagnoses*2,floor(rpois(N,0.12)*8*logistic((age_score-1)/4))))
   max_glu_serum <- sample(c('>200','>300','None','Norm'), N, replace = T, prob = c(0.015,0.012,0.947,0.026))
   A1Cresult <- sample(c('>7','>8','None','Norm'), N, replace = T, prob = c(0.037,0.081,0.833,0.049))
   insulin <- sample(c('Down','No','Steady','Up'), N, replace = T, prob = c(0.12,0.466,0.303,.111))
+  insulin_score <- sample(1:4,N,replace=T,prob = c(0.12,0.466,0.303,.111) )
+  insulin <- ifelse(insulin_score==1,'Down',(ifelse(insulin_score==2,'No',
+                       (ifelse(insulin_score==3,'Steady','Up')))))
   change <- sample(c('Ch','No'), N, replace = T, prob = c(0.462,0.538))
+  change_score <- sample(1:2,N,replace=T,prob = c(0.462,0.538))
+  change <- ifelse(change_score==1,'Ch','No')
   diabetesMed <- sample(c('No','Yes'), N, replace = T, prob = c(0.23,0.77))
+  diabetesMed_score <- sample(1:2,N,replace=T,prob = c(0.23,0.77))
+  diabetesMed <- ifelse(diabetesMed_score==1,'No','Yes')
   diag1 <- sample(c('circulatory','respiratory','digestive','diabetes','injury',
                     'musculoskeletal','genitourinary','neoplasms','other'), N, replace = T, 
                   prob = c(.299,.142,.093,.086,.068,.049,.05,.138,.074))
-  diag2 <- sample(c('circulatory','respiratory','digestive','diabetes','injury',
-                    'musculoskeletal','genitourinary','neoplasms','other'), N, replace = T, 
-                  prob = c(.313,.107,.041,.126,.024,.017,.082,.185,.105))
+  #diag1_score <- sample(1:9,N,replace=T,prob = c(.299,.142,.093,.086,.068,.049,.05,.138,.074))
   admission_source <- sample(c('clinic_referral','emergency','other'), N, replace = T, prob = c(0.101,0.565,.334))
   discharged_to <- sample(c('home','transferred','left_AMA'), N, replace = T, prob = c(0.241,0.753,.006))
   payer_code <- sample(c('Insured','Self_pay'), N, replace = T, prob = c(0.951,0.049))
-  readmitted <- sample(c('<30','>30','NO'), N, replace = T, prob = c(0.538,0.462))
-  data.frame(age,gender,race,time_in_hospital,num_lab_procedures,num_procedures,
+  score <- 0.67*age_score + 0.5*time_in_hospital + 0.5 * num_lab_procedures -
+    0.49 *num_procedures + 0.5 *num_medications + 0.52 *number_outpatient +
+    0.56 *number_emergency + 0.6 *number_inpatient + 0.5 *number_diagnoses - 
+    1.43 * insulin_score + 0.9 * change_score + 0.6 * diabetesMed_score
+  class <- runif(length(score))< .469*logistic((score-30)/2)
+  readmitted <- ifelse(class==T,"YES","NO")
+  data.frame(gender,race,age,time_in_hospital,num_lab_procedures,num_procedures,
              num_medications,number_outpatient,number_emergency,number_inpatient,number_diagnoses,
-             max_glu_serum,A1Cresult,insulin,change,diabetesMed,diag1,diag2,
+             max_glu_serum,A1Cresult,insulin,change,diabetesMed,diag1,
              admission_source,discharged_to,payer_code,readmitted)
 }
 
 
-  
-number_diagnoses
+data <- generate_dataset(N)
+
+
+fit_sim <- glm(readmitted~.,data=data, family=binomial)
+summary(fit_sim)
 
 
 
-prop.table(table(data2$number_diagnoses))
-
-sd(data2$num_procedures)
-mean(data2$num_procedures)
-plot(data2$discharged_to) # transferred to another facility 70%
-hist(data2$num_procedures)
-hist(raw$num_procedures)
-hist(num_procedures)
-
-set.seed(123)
-num_proceduress <- rnorm(10000,43.1,19.67)
-hist(num_procedures)
-num_lab_procedures <- floor(age * rpois(10000,34))/4.5
-hist(num_lab_procedures)
-num_lab_procedures <- floor(age * runif(10000,1,132))/12
-hist(num_lab_procedures)
-set.seed(123)
-num_procedures <- rpois(10000,1.4)*1.2*logistic((age-5)/.8)
-hist(num_procedures)
-plot(age,num_procedures)
-
-df <- data.frame(age,time_in_hospital)
-
-summary(data2)
-
-mean(age)
-age <- sample(1:10, 10000, replace=T, prob=c(0.001, 0.007, 0.016, 0.037, 0.095, 0.17, 0.221, 0.257, 0.169, 0.027))
-space <- runif(1000, 1, 10)
-plot(data2$age)
 
 
-library(ggplot2)
-
-g <- ggplot(data2, aes(x=age, y=num_procedures))
-g + geom_boxplot()
-
-aov1 <- aov(data2$time_in_hospital ~ data2$age)
-summary(aov1)
-
-fit <- lm(class~., data = data2)
-summary(fit)
-plot(df$age,df$time_in_hospital)
-
-data3 <- data2
-data3$class[data3$readmitted=='<30'] <- 1
-data3$class[data3$readmitted=='>30'] <- 2
-data3$class[data3$readmitted=='NO'] <- 3
-data3  
